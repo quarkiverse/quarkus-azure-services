60971a7cd086c8c681aeb9e484424e56a464e3f2
Jianguo Ma <jiangma@microsoft.com>
2022-10-13 18:52:13 +0800

    add unit tests
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ storage-blob/deployment/src/test/java/io/quarkiverse/azureservices/azure/storage/blob/deployment/AzureStorageBlobTest.java
@@ -0,0 +1,34 @@
+package io.quarkiverse.azureservices.azure.storage.blob.deployment;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.azure.storage.blob.BlobServiceClient;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.QuarkusTestResource;
+
+@QuarkusTestResource(AzureStorageBlobTestResource.class)
+public class AzureStorageBlobTest {
+
+    @Inject
+    Instance<BlobServiceClient> blobServiceClient;
+
+    @RegisterExtension
+    static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .overrideConfigKey("quarkus.azure.storage.blob.connection-string",
+                    "${quarkus.azure.storage.blob.connection-string}");
+
+    @Test
+    public void test() {
+        assertNotNull(blobServiceClient.get());
+    }
+}
