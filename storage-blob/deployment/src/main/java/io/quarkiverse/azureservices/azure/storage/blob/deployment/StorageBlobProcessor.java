2456f75afe8714c541df3206daf38e356b2d4673
Jianguo Ma <jiangma@microsoft.com>
2022-10-29 20:30:54 +0800

    support native image build for azure-storage-blob
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- storage-blob/deployment/src/main/java/io/quarkiverse/azureservices/azure/storage/blob/deployment/StorageBlobProcessor.java
+++ storage-blob/deployment/src/main/java/io/quarkiverse/azureservices/azure/storage/blob/deployment/StorageBlobProcessor.java
@@ -1,9 +1,19 @@
 package io.quarkiverse.azureservices.azure.storage.blob.deployment;
 
+import java.util.Arrays;
+
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
+
 import io.quarkiverse.azureservices.azure.storage.blob.runtime.StorageBlobServiceClientProducer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageProxyDefinitionBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 class StorageBlobProcessor {
 
@@ -15,7 +25,40 @@ class StorageBlobProcessor {
     }
 
     @BuildStep
-    public AdditionalBeanBuildItem producer() {
+    AdditionalBeanBuildItem producer() {
         return new AdditionalBeanBuildItem(StorageBlobServiceClientProducer.class);
+    }
+
+    @BuildStep
+    IndexDependencyBuildItem indexDependency() {
+        return new IndexDependencyBuildItem("com.azure", "azure-storage-blob");
+    }
+
+    @BuildStep
+    void reflectiveClasses(CombinedIndexBuildItem combinedIndex, BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {
+        String[] modelClasses = combinedIndex
+                .getIndex()
+                .getKnownClasses()
+                .stream()
+                .map(ClassInfo::name)
+                .map(DotName::toString)
+                .filter(n -> n.startsWith("com.azure.storage.blob.implementation.models.")
+                        || n.startsWith("com.azure.storage.blob.models."))
+                .sorted()
+                .toArray(String[]::new);
+        modelClasses = Arrays.copyOf(modelClasses, modelClasses.length + 1);
+        modelClasses[modelClasses.length - 1] = "com.azure.core.http.rest.ResponseBase";
+        reflectiveClasses.produce(new ReflectiveClassBuildItem(false, true, modelClasses));
+    }
+
+    @BuildStep
+    NativeImageProxyDefinitionBuildItem proxies() {
+        return new NativeImageProxyDefinitionBuildItem(
+                "com.azure.storage.blob.implementation.ServicesImpl$ServicesService",
+                "com.azure.storage.blob.implementation.ContainersImpl$ContainersService",
+                "com.azure.storage.blob.implementation.BlobsImpl$BlobsService",
+                "com.azure.storage.blob.implementation.PageBlobsImpl$PageBlobsService",
+                "com.azure.storage.blob.implementation.BlockBlobsImpl$BlockBlobsService",
+                "com.azure.storage.blob.implementation.AppendBlobsImpl$AppendBlobsService");
     }
 }
