c3325dacf83165613f21b31dfaa6fce87e837ac8
Peter Palaga <ppalaga@redhat.com>
2022-11-11 13:53:49 +0100

    Reformat StorageBlobServiceClientProducer

--- storage-blob/runtime/src/main/java/io/quarkiverse/azureservices/azure/storage/blob/runtime/StorageBlobServiceClientProducer.java
+++ storage-blob/runtime/src/main/java/io/quarkiverse/azureservices/azure/storage/blob/runtime/StorageBlobServiceClientProducer.java
@@ -1,11 +1,11 @@
 package io.quarkiverse.azureservices.azure.storage.blob.runtime;
 
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 
 public class StorageBlobServiceClientProducer {
 
