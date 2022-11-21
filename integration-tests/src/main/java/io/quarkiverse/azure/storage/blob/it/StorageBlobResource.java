b2ec31d6ef5407eb5728ea58fa9b132997b7fec4
Peter Palaga <ppalaga@redhat.com>
2022-11-21 16:10:42 +0100

    Test updating, listing and deleting blobs in addition to creating and reading

--- integration-tests/src/main/java/io/quarkiverse/azure/storage/blob/it/StorageBlobResource.java
+++ integration-tests/src/main/java/io/quarkiverse/azure/storage/blob/it/StorageBlobResource.java
@@ -18,13 +18,16 @@ package io.quarkiverse.azure.storage.blob.it;
 
 import static javax.ws.rs.core.Response.Status.CREATED;
 
-import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,6 +36,7 @@ import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.BlobItem;
 
 @Path("/quarkus-azure-storage-blob")
 @ApplicationScoped
@@ -41,23 +45,59 @@ public class StorageBlobResource {
     @Inject
     BlobServiceClient blobServiceClient;
 
+    @Path("/{container}/{blobName}")
     @POST
-    public Response uploadBlob() {
-        BlobContainerClient blobContainerClient = blobServiceClient
-                .createBlobContainerIfNotExists("container-quarkus-azure-storage-blob");
-        BlobClient blobClient = blobContainerClient.getBlobClient("quarkus-azure-storage-blob.txt");
-        blobClient.upload(BinaryData.fromString("Hello quarkus-azure-storage-blob at " + LocalDateTime.now()), true);
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response uploadBlob(
+            @PathParam("container") String container,
+            @PathParam("blobName") String blobName,
+            String body) {
+        BlobContainerClient blobContainerClient = blobServiceClient.createBlobContainerIfNotExists(container);
+        BlobClient blobClient = blobContainerClient.getBlobClient(blobName);
+        blobClient.upload(BinaryData.fromString(body), true);
 
         return Response.status(CREATED).build();
     }
 
+    @Path("/{container}/{blobName}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String downloadBlob() {
+    public Response downloadBlob(
+            @PathParam("container") String container,
+            @PathParam("blobName") String blobName) {
+        BlobContainerClient blobContainerClient = blobServiceClient.createBlobContainerIfNotExists(container);
+        BlobClient blobClient = blobContainerClient.getBlobClient(blobName);
+
+        if (blobClient.exists()) {
+            return Response.ok().entity(blobClient.downloadContent().toString()).build();
+        } else {
+            return Response.status(404).build();
+        }
+
+    }
+
+    @Path("/{container}/{blobName}")
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response delete(
+            @PathParam("container") String container,
+            @PathParam("blobName") String blobName) {
+        BlobContainerClient blobContainerClient = blobServiceClient
+                .createBlobContainerIfNotExists(container);
+        blobContainerClient.getBlobClient(blobName).delete();
+
+        return Response.noContent().build();
+    }
+
+    @Path("/{container}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String listBlobs() {
         BlobContainerClient blobContainerClient = blobServiceClient
                 .createBlobContainerIfNotExists("container-quarkus-azure-storage-blob");
-        BlobClient blobClient = blobContainerClient.getBlobClient("quarkus-azure-storage-blob.txt");
-
-        return blobClient.downloadContent().toString();
+        return blobContainerClient.listBlobs().stream()
+                .map(BlobItem::getName)
+                .collect(Collectors.joining(","));
     }
+
 }
--- integration-tests/src/test/java/io/quarkiverse/azure/storage/blob/it/StorageBlobResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/azure/storage/blob/it/StorageBlobResourceTest.java
@@ -1,10 +1,13 @@
 package io.quarkiverse.azure.storage.blob.it;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -15,21 +18,60 @@ import io.quarkus.test.junit.QuarkusTest;
 class StorageBlobResourceTest {
 
     @Test
-    @Order(1)
     void shouldUploadATextfile() {
-        given()
-                .when().post("/quarkus-azure-storage-blob")
-                .then()
-                .statusCode(201);
-    }
 
-    @Test
-    @Order(2)
-    void shouldDownloadATextfile() {
-        given()
-                .when().get("/quarkus-azure-storage-blob")
-                .then()
-                .statusCode(200)
-                .body(startsWith("Hello quarkus-azure-storage-blob"));
+        final String container = "container-quarkus-azure-storage-blob";
+        final String blobName = "StorageBlobResourceTest-" + UUID.randomUUID().toString() + ".txt";
+        final String body = "Hello quarkus-azure-storage-blob at " + LocalDateTime.now();
+
+        try {
+            // Create
+            given()
+                    .when()
+                    .body(body)
+                    .post("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(201);
+
+            // Read
+            given()
+                    .when().get("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(200)
+                    .body(is(body));
+
+            // List
+            given()
+                    .when().get("/quarkus-azure-storage-blob/" + container)
+                    .then()
+                    .statusCode(200)
+                    .body(containsString(blobName)); // there may exist blobs created by other runs of this test
+
+            // Update
+            final String updatedBody = body + " updated";
+            given()
+                    .when()
+                    .body(updatedBody)
+                    .post("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(201);
+
+            given()
+                    .when().get("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(200)
+                    .body(is(updatedBody));
+        } finally {
+            given()
+                    .when()
+                    .delete("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(204);
+
+            given()
+                    .when().get("/quarkus-azure-storage-blob/" + container + "/" + blobName)
+                    .then()
+                    .statusCode(404);
+        }
     }
 }
