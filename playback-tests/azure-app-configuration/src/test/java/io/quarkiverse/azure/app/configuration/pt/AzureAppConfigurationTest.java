ff03bdc82dc216752752a0b62b6ddabe2445b967
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-02-03 15:56:01 +0100

    Comments

--- playback-tests/azure-app-configuration/src/test/java/io/quarkiverse/azure/app/configuration/pt/AzureAppConfigurationTest.java
+++ playback-tests/azure-app-configuration/src/test/java/io/quarkiverse/azure/app/configuration/pt/AzureAppConfigurationTest.java
@@ -1,15 +1,19 @@
 package io.quarkiverse.azure.app.configuration.pt;
 
+import com.azure.core.test.TestBase;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.jupiter.api.Test;
-
-import com.azure.core.test.TestBase;
-
-import io.quarkus.test.junit.QuarkusTest;
-
+/**
+ * When executed in RECORD mode, the entire Azure environment needs to be really created. That means creating the resource group, the App Configuration and the key/values used in this test.
+ * 
+ * az appconfig kv set --name $APP_CONFIG_NAME --yes --key my.prop --value "1234"
+ * az appconfig kv set --name $APP_CONFIG_NAME --yes --key another.prop --value "5678"
+ */
 @QuarkusTest
 class AzureAppConfigurationTest extends TestBase {
     @Test
