08bd06b06b0e8587a6c7490cd499e0cdab2e0146
Jianguo Ma <jiangma@microsoft.com>
2023-05-26 09:04:18 +0800

    bump azurite to 3.23.0 to support Azure Storage API Version 2022-11-02
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- extensions/azure-storage-blob/deployment/src/test/java/io/quarkiverse/azure/storage/blob/deployment/StorageBlobTestResource.java
+++ extensions/azure-storage-blob/deployment/src/test/java/io/quarkiverse/azure/storage/blob/deployment/StorageBlobTestResource.java
@@ -11,7 +11,7 @@ public class StorageBlobTestResource implements QuarkusTestResourceLifecycleMana
 
     static String accountName = "devstoreaccount1";
     static String accountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
-    static String image = "mcr.microsoft.com/azure-storage/azurite:3.21.0";
+    static String image = "mcr.microsoft.com/azure-storage/azurite:3.23.0";
     static int port = 10000;
     static String protocol = "http";
 
