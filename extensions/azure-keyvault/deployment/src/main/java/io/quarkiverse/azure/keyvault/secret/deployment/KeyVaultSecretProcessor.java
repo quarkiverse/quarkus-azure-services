5e2aca588d489581810b69ae0302b5c8a9711a53
galiacheng <haixia.cheng@microsoft.com>
2024-04-16 08:58:06 +0000

    add dynamic-proxy.

--- extensions/azure-keyvault/deployment/src/main/java/io/quarkiverse/azure/keyvault/secret/deployment/KeyVaultSecretProcessor.java
+++ extensions/azure-keyvault/deployment/src/main/java/io/quarkiverse/azure/keyvault/secret/deployment/KeyVaultSecretProcessor.java
@@ -1,7 +1,11 @@
 package io.quarkiverse.azure.keyvault.secret.deployment;
 
+import java.util.List;
+
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
+
+import com.azure.core.annotation.ServiceInterface;
 
 import io.quarkiverse.azure.keyvault.secret.runtime.KeyVaultSecretClientProducer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
@@ -11,11 +15,13 @@ import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageProxyDefinitionBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 class KeyVaultSecretProcessor {
 
     static final String FEATURE = "azure-keyvault-secret";
+    private static final DotName SERVICE_INTERFACE_DOT_NAME = DotName.createSimple(ServiceInterface.class.getName());
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -50,5 +56,22 @@ class KeyVaultSecretProcessor {
                 .sorted()
                 .toArray(String[]::new);
         reflectiveClasses.produce(ReflectiveClassBuildItem.builder(modelClasses).fields().build());
+    }
+
+    @BuildStep
+    void proxyDefinitions(
+            CombinedIndexBuildItem combinedIndex,
+            BuildProducer<NativeImageProxyDefinitionBuildItem> proxyDefinitions) {
+
+        combinedIndex
+                .getIndex()
+                .getAnnotations(SERVICE_INTERFACE_DOT_NAME)
+                .stream()
+                .map(annotationInstance -> annotationInstance.target().asClass().name().toString())
+                .map(NativeImageProxyDefinitionBuildItem::new)
+                .forEach(proxyDefinitions::produce);
+
+        proxyDefinitions.produce(new NativeImageProxyDefinitionBuildItem(
+                List.of("com.azure.security.keyvault.secrets.implementation.SecretClientImpl$SecretClientService")));
     }
 }
---
+++ internal/azure-identity/deployment/src/main/java/io/quarkiverse/azure/identity/deployment/AzureIdentityProcessor.java
@@ -0,0 +1,45 @@
+package io.quarkiverse.azure.identity.deployment;
+
+import java.util.stream.Stream;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.pkg.builditem.NativeImageRunnerBuildItem;
+import io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild;
+
+public class AzureIdentityProcessor {
+    private static final String AZURE_IDENTITY_NAME = "azure-identity";
+
+    @BuildStep(onlyIf = NativeOrNativeSourcesBuild.class)
+    void runtimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClass) {
+        // CRT and all types that statically init CRT
+        Stream.of(
+                "com.microsoft.aad.msal4jextensions.persistence.linux.ISecurityLibrary",
+                "com.microsoft.aad.msal4jextensions.persistence.mac.ISecurityLibrary")
+                .map(RuntimeInitializedClassBuildItem::new)
+                .forEach(runtimeInitializedClass::produce);
+    }
+
+    @BuildStep(onlyIf = NativeOrNativeSourcesBuild.class)
+    public void resources(
+            BuildProducer<NativeImageResourceBuildItem> resource,
+            NativeImageRunnerBuildItem nativeImageRunnerFactory) {
+
+        String dir = "QMETA-INF/services";
+        String hotspotLibName = "jdk.vm.ci.hotspot.HotSpotJVMCIBackendFactory";
+        String jvmCiServiceLocatorLibName = "jdk.vm.ci.services.JVMCIServiceLocator";
+        String hotspotLibPath = dir + "/" + hotspotLibName;
+        String jvmCiServiceLocatorLibPath = dir + "/" + jvmCiServiceLocatorLibName;
+
+        resource.produce(new NativeImageResourceBuildItem(hotspotLibPath));
+        resource.produce(new NativeImageResourceBuildItem(jvmCiServiceLocatorLibPath));
+
+        String jnaDir = "Qcom/sun/jna/linux-x86-64";
+        String jniLibName = "libjnidispatch.so";
+        String jniLibPath = jnaDir + "/" + jniLibName;
+
+        resource.produce(new NativeImageResourceBuildItem(jniLibPath));
+    }
+}
