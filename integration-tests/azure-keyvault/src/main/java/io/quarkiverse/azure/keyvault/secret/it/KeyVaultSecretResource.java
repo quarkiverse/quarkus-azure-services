b68c60b55a94fb6c63f7bcf2a23fda115d5bb834
galiacheng <haixia.cheng@microsoft.com>
2024-04-26 05:08:32 +0000

    remove unused import.

--- integration-tests/azure-keyvault/src/main/java/io/quarkiverse/azure/keyvault/secret/it/KeyVaultSecretResource.java
+++ integration-tests/azure-keyvault/src/main/java/io/quarkiverse/azure/keyvault/secret/it/KeyVaultSecretResource.java
@@ -3,7 +3,6 @@ package io.quarkiverse.azure.keyvault.secret.it;
 import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
