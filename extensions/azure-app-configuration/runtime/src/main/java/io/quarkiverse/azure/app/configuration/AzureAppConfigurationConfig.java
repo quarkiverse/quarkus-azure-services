46b6b480d8d361a3fbef1692cad84510bb47b2ed
Jianguo Ma <jiangma@microsoft.com>
2024-05-08 18:15:58 +0800

    add config property "quarkus.azure.app.configuration.enabled"

--- extensions/azure-app-configuration/runtime/src/main/java/io/quarkiverse/azure/app/configuration/AzureAppConfigurationConfig.java
+++ extensions/azure-app-configuration/runtime/src/main/java/io/quarkiverse/azure/app/configuration/AzureAppConfigurationConfig.java
@@ -5,24 +5,37 @@ import java.util.Optional;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 @ConfigMapping(prefix = "quarkus.azure.app.configuration")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
 public interface AzureAppConfigurationConfig {
+
+    /** The flag to enable the app configuration. If set to false, the app configuration will be disabled */
+    @WithDefault("true")
+    boolean enabled();
+
     /** The endpoint of the app configuration */
-    String endpoint();
+    Optional<String> endpoint();
 
     /** The id of the app configuration */
-    String id();
+    Optional<String> id();
 
     /** The secret of the app configuration */
-    String secret();
+    Optional<String> secret();
 
     /** The label filter of the app configuration. Use comma as separator for multiple label names */
     Optional<String> labels();
 
     /** The connection string */
     default String connectionString() {
-        return "Endpoint=" + endpoint() + ";Id=" + id() + ";Secret=" + secret();
+        if (enabled()) {
+            assert endpoint().isPresent() : "The endpoint of the app configuration must be set";
+            assert id().isPresent() : "The id of the app configuration must be set";
+            assert secret().isPresent() : "The secret of the app configuration must be set";
+            return "Endpoint=" + endpoint().get() + ";Id=" + id().get() + ";Secret=" + secret().get();
+        } else {
+            return null;
+        }
     }
 }
