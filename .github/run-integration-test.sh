45becc49a4f23c2f28676e085a9bbac6e46e14c6
Jianguo Ma <jiangma@microsoft.com>
2024-12-20 11:23:47 +0800

    run integration-tests with native executable and in jvm mode separately for each extension 1. observed that once 'mvn verify' is executed, invoking 'mvn test-compile failsafe:integration-test -Dnative' always executes integration tests in jvm mode even though there is native executable
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- .github/run-integration-test.sh
+++ .github/run-integration-test.sh
@@ -7,11 +7,13 @@ set -Euo pipefail
 # - APP_CONFIG_NAME
 # - KEY_VAULT_NAME
 # - COSMOSDB_ACCOUNT_NAME
+# - EVENTHUBS_NAMESPACE
+# - EVENTHUBS_EVENTHUB_NAME
 
 # Azure Storage Blob Extension
-# Authenticate to Azure Storage Blob with Microsoft Entra ID
-# Export the endpoint of azure storage blob
-export QUARKUS_AZURE_STORAGE_BLOB_ENDPOINT=$(az storage account show \
+# Authenticate to Azure Storage Blob with Microsoft Entra ID and connection string
+# Get the endpoint of azure storage blob
+AZURE_STORAGE_BLOB_ENDPOINT=$(az storage account show \
     --resource-group $RESOURCE_GROUP_NAME \
     --name $STORAGE_ACCOUNT_NAME \
     --query 'primaryEndpoints.blob' \
@@ -23,10 +25,32 @@ STORAGE_ACCOUNT_RESOURCE_ID=$(az storage account show \
     --query 'id' \
     --output tsv)
 # Assign the "Storage Blob Data Contributor" role to the current signed-in identity
+servicePrincipal=$(az ad sp list --filter "appId eq '$AZURE_CLIENT_ID'" --query '[0].id' -o tsv)
 az role assignment create \
-    --assignee $(az ad signed-in-user show --query 'id' --output tsv) \
+    --assignee ${servicePrincipal} \
     --role "Storage Blob Data Contributor" \
     --scope $STORAGE_ACCOUNT_RESOURCE_ID
+# Get the connection string that has full access to the account
+AZURE_STORAGE_BLOB_CONNECTION_STRING=$(az storage account show-connection-string \
+    --resource-group "${RESOURCE_GROUP_NAME}" \
+    --name "${STORAGE_ACCOUNT_NAME}" \
+    --query connectionString -o tsv)
+
+# Run integration test with existing native executables against Azure services
+export QUARKUS_AZURE_STORAGE_BLOB_ENDPOINT=${AZURE_STORAGE_BLOB_ENDPOINT}
+unset QUARKUS_AZURE_STORAGE_BLOB_CONNECTION_STRING
+mvn -f azure-storage-blob/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+export QUARKUS_AZURE_STORAGE_BLOB_CONNECTION_STRING=${AZURE_STORAGE_BLOB_CONNECTION_STRING}
+unset QUARKUS_AZURE_STORAGE_BLOB_ENDPOINT
+mvn -f azure-storage-blob/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+
+# Run both unit test and integration test in JVM mode against Azure services
+export QUARKUS_AZURE_STORAGE_BLOB_ENDPOINT=${AZURE_STORAGE_BLOB_ENDPOINT}
+unset QUARKUS_AZURE_STORAGE_BLOB_CONNECTION_STRING
+mvn -f azure-storage-blob/pom.xml -B verify -Dazure.test=true
+export QUARKUS_AZURE_STORAGE_BLOB_CONNECTION_STRING=${AZURE_STORAGE_BLOB_CONNECTION_STRING}
+unset QUARKUS_AZURE_STORAGE_BLOB_ENDPOINT
+mvn -f azure-storage-blob/pom.xml -B verify -Dazure.test=true
 
 # Azure App Configuration Extension
 export QUARKUS_AZURE_APP_CONFIGURATION_ENDPOINT=$(az appconfig show \
@@ -39,15 +63,19 @@ credential=$(az appconfig credential list \
     | jq 'map(select(.readOnly == true)) | .[0]')
 export QUARKUS_AZURE_APP_CONFIGURATION_ID=$(echo "${credential}" | jq -r '.id')
 export QUARKUS_AZURE_APP_CONFIGURATION_SECRET=$(echo "${credential}" | jq -r '.value')
+mvn -f azure-app-configuration/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+mvn -f azure-app-configuration/pom.xml -B verify -Dazure.test=true
 
 # Azure Key Vault Extension
 export QUARKUS_AZURE_KEYVAULT_SECRET_ENDPOINT=$(az keyvault show --name "${KEY_VAULT_NAME}" \
     --resource-group "${RESOURCE_GROUP_NAME}" \
     --query properties.vaultUri\
     --output tsv)
+mvn -f azure-keyvault/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+mvn -f azure-keyvault/pom.xml -B verify -Dazure.test=true
 
 # Azure Cosmos Extension
-# Authenticate to Azure Cosmos DB with Microsoft Entra ID
+# Authenticate to Azure Cosmos DB with Microsoft Entra ID and key
 # Export the endpoint of azure cosmos db
 export QUARKUS_AZURE_COSMOS_ENDPOINT=$(az cosmosdb show \
     -n ${COSMOSDB_ACCOUNT_NAME} \
@@ -65,7 +93,6 @@ az cosmosdb sql container create \
     -n democontainer \
     -p "/id"
 # Assign the "Cosmos DB Data Contributor" role to the current signed-in identity
-servicePrincipal=$(az ad sp list --filter "appId eq '$AZURE_CLIENT_ID'" --query '[0].id' -o tsv)
 az cosmosdb sql role assignment create \
     --account-name ${COSMOSDB_ACCOUNT_NAME} \
     --resource-group ${RESOURCE_GROUP_NAME} \
@@ -104,12 +131,18 @@ mvn -B verify -Dazure.test=true
 # Run integration test again for Azure Cosmos Extension by using key for authentication
 # Export the key that has full access to the account including management plane and data plane operations
 export QUARKUS_AZURE_COSMOS_KEY=$(az cosmosdb keys list \
+# Get the key that has full access to the account including management plane and data plane operations
+AZURE_COSMOS_KEY=$(az cosmosdb keys list \
     -n ${COSMOSDB_ACCOUNT_NAME} \
     -g ${RESOURCE_GROUP_NAME} \
     --query primaryMasterKey -o tsv)
-cd ../azure-cosmos
-mvn -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
-mvn -B verify -Dazure.test=true
 
-# Change the directory back to integration-tests
-cd ..
+unset QUARKUS_AZURE_COSMOS_KEY
+mvn -f azure-cosmos/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+export QUARKUS_AZURE_COSMOS_KEY=${AZURE_COSMOS_KEY}
+mvn -f azure-cosmos/pom.xml -B test-compile failsafe:integration-test -Dnative -Dazure.test=true
+
+unset QUARKUS_AZURE_COSMOS_KEY
+mvn -f azure-cosmos/pom.xml -B verify -Dazure.test=true
+export QUARKUS_AZURE_COSMOS_KEY=${AZURE_COSMOS_KEY}
+mvn -f azure-cosmos/pom.xml -B verify -Dazure.test=true
