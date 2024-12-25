9b02b4695d522b1123bc5d024662df53b14cd3e0
Jianguo Ma <jiangma@microsoft.com>
2024-12-25 20:51:35 +0800

    add EventHubsDisabled tests in integration-tests/azure-services-disabled
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ integration-tests/azure-services-disabled/src/test/java/io/quarkiverse/azure/services/disabled/it/EventHubsDisabledTest.java
@@ -0,0 +1,48 @@
+package io.quarkiverse.azure.services.disabled.it;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+import static org.hamcrest.Matchers.equalTo;
+
+@QuarkusTest
+public class EventHubsDisabledTest {
+
+    @Test
+    public void testGetEventHubProducerClient() {
+        given()
+                .get("/quarkus-eventhubs-disabled/eventHubProducerClient")
+                .then()
+                .statusCode(NOT_FOUND.getStatusCode())
+                .body(equalTo("The EventHubProducerClient is null because the Azure Event Hubs is disabled"));
+    }
+
+    @Test
+    public void testGetEventHubConsumerClient() {
+        given()
+                .get("/quarkus-eventhubs-disabled/eventHubConsumerClient")
+                .then()
+                .statusCode(NOT_FOUND.getStatusCode())
+                .body(equalTo("The EventHubConsumerClient is null because the Azure Event Hubs is disabled"));
+    }
+
+    @Test
+    public void testGetEventHubProducerAsyncClient() {
+        given()
+                .get("/quarkus-eventhubs-disabled/eventHubProducerAsyncClient")
+                .then()
+                .statusCode(NOT_FOUND.getStatusCode())
+                .body(equalTo("The EventHubProducerAsyncClient is null because the Azure Event Hubs is disabled"));
+    }
+
+    @Test
+    public void testGetEventHubConsumerAsyncClient() {
+        given()
+                .get("/quarkus-eventhubs-disabled/eventHubConsumerAsyncClient")
+                .then()
+                .statusCode(NOT_FOUND.getStatusCode())
+                .body(equalTo("The EventHubConsumerAsyncClient is null because the Azure Event Hubs is disabled"));
+    }
+}
