a4b9e9d083510aac69efcf8dc0c4f817cbb16cb1
Jianguo Ma <jiangma@microsoft.com>
2025-01-20 15:47:16 +0800

    fast delete resources without purge
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- .github/delete-azure-resources.sh
+++ .github/delete-azure-resources.sh
@@ -2,13 +2,5 @@
 
 # The following environment variables need to be configured before running the script
 # - RESOURCE_GROUP_NAME
-# - APP_CONFIG_NAME
-# - KEY_VAULT_NAME
-# - EVENTHUBS_NAMESPACE
 
-az appconfig delete --name "${APP_CONFIG_NAME}" --resource-group "${RESOURCE_GROUP_NAME}" --yes
-az appconfig purge --name "${APP_CONFIG_NAME}" --yes
-az keyvault delete --name "${KEY_VAULT_NAME}" --resource-group "${RESOURCE_GROUP_NAME}"
-az keyvault purge --name "${KEY_VAULT_NAME}"
-az eventhubs namespace delete --resource-group "${RESOURCE_GROUP_NAME}" --name "${EVENTHUBS_NAMESPACE}"
 az group delete --name "${RESOURCE_GROUP_NAME}" --yes --no-wait
