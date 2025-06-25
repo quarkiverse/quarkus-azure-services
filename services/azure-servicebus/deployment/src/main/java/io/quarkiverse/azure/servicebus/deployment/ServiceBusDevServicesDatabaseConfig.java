52570badfd4829f166d71049a2fe1a05e3fd1077
Harald Albers <harald.albers@hamburger-software.de>
2025-06-25 09:59:40 +0200

    Use latest tags in container images but recommend specific versions

--- services/azure-servicebus/deployment/src/main/java/io/quarkiverse/azure/servicebus/deployment/ServiceBusDevServicesDatabaseConfig.java
+++ services/azure-servicebus/deployment/src/main/java/io/quarkiverse/azure/servicebus/deployment/ServiceBusDevServicesDatabaseConfig.java
@@ -3,7 +3,7 @@ package io.quarkiverse.azure.servicebus.deployment;
 import io.smallrye.config.WithDefault;
 
 public interface ServiceBusDevServicesDatabaseConfig {
-    String DEFAULT_IMAGE_NAME = "mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04";
+    String DEFAULT_IMAGE_NAME = "mcr.microsoft.com/mssql/server:latest";
 
     String PREFIX = ServiceBusDevServicesConfig.PREFIX + ".database";
     /**
@@ -16,6 +16,13 @@ public interface ServiceBusDevServicesDatabaseConfig {
      * The container image name of the Microsoft SQL Server required by the Azure Service Bus emulator.
      * See the <a href="https://mcr.microsoft.com/en-us/artifact/mar/mssql/server/tags">artifact
      * registry</a> for available tags of the default image.
+     * <p>
+     * The default image uses the {@code latest} tag.
+     * This can lead to build failures if a new incompatible version is published.
+     * For stability, it is recommended to specify an explicit version tag.
+     * <p>
+     * This extension has been tested and verified working with version
+     * {@code mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04}.
      */
     @WithDefault(DEFAULT_IMAGE_NAME)
     String imageName();
--- services/azure-servicebus/deployment/src/main/java/io/quarkiverse/azure/servicebus/deployment/ServiceBusDevServicesEmulatorConfig.java
+++ services/azure-servicebus/deployment/src/main/java/io/quarkiverse/azure/servicebus/deployment/ServiceBusDevServicesEmulatorConfig.java
@@ -3,7 +3,7 @@ package io.quarkiverse.azure.servicebus.deployment;
 import io.smallrye.config.WithDefault;
 
 public interface ServiceBusDevServicesEmulatorConfig {
-    String DEFAULT_IMAGE_NAME = "mcr.microsoft.com/azure-messaging/servicebus-emulator:1.1.2";
+    String DEFAULT_IMAGE_NAME = "mcr.microsoft.com/azure-messaging/servicebus-emulator:latest";
 
     String PREFIX = ServiceBusDevServicesConfig.PREFIX + ".emulator";
     /**
@@ -16,6 +16,13 @@ public interface ServiceBusDevServicesEmulatorConfig {
      * The container image name of the Azure Service Bus emulator.
      * See the <a href="https://mcr.microsoft.com/en-us/artifact/mar/azure-messaging/servicebus-emulator/tags">artifact
      * registry</a> for available tags of the default image.
+     * <p>
+     * The default image uses the {@code latest} tag.
+     * This can lead to build failures if a new incompatible version is published.
+     * For stability, it is recommended to specify an explicit version tag.
+     * <p>
+     * This extension has been tested and verified working with version
+     * {@code mcr.microsoft.com/azure-messaging/servicebus-emulator:1.1.2}.
      */
     @WithDefault(DEFAULT_IMAGE_NAME)
     String imageName();
